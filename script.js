4aa8a1af9e974c86540126e1162ee75255a28b83
manasipatil0109 <patilm0204@gmail.com>
2021-07-30 17:19:20 +0530

    init commit

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-function valueButton(e){
-    calculatorForm.screen.value += e.value;
+var flag=-1;
+function valueButton(e,num)
+{
+    if(flag==-1 && (num==0 || num==2))
+       calculatorForm.screen.value += e.value;
+    else if((flag==0) || ((flag==1 || flag==2) && (num==0)))
+       calculatorForm.screen.value += e.value;
+    else
+    {
+        alert("Please Enter valid symbol and number");
+    }
+    flag=num;
 }
 function Clear(){
     calculatorForm.screen.value = null;
 }
 function Calculate(){
+    if(calculatorForm.screen.value=="")
+    {
+        alert("Please enter the values");
+    }
+    else{
     calculatorForm.screen.value = eval(calculatorForm.screen.value);
+    }
 }
 function Delete_func() {
     calculatorForm.screen.value = calculatorForm.screen.value.substr(0,calculatorForm.screen.value.length-1)
